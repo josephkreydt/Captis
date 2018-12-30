2d36029b79228f1a3cd41f54adc5d741b1a9ff8b
Joe <8309575+joekreydt@users.noreply.github.com>
2018-12-30 05:43:16 -0500

    Uploaded first part of application
    This gets the MediaStream going, but does not yet have a function to capture the stream.

---
+++ app.js
@@ -0,0 +1,54 @@
+document.getElementById('startRecording').addEventListener('click', recordClick, false);
+
+//https://developer.chrome.com/extensions/desktopCapture
+function recordClick() {
+    chrome.desktopCapture.chooseDesktopMedia(["screen", "window", "tab", "audio"], accessToRecord);
+}
+
+// Supported audio constraints: https://addpipe.com/blog/audio-constraints-getusermedia/
+function accessToRecord(id, audioState) {
+    // if statement checks the canRequestAudioTrack object of the audioState argument given to accessToRecord()
+    if (audioState.canRequestAudioTrack) {
+        console.log("Audio is being recorded");
+    } else {
+        console.log("Audio is not being recorded.")
+    }
+    navigator.mediaDevices.getUserMedia({
+        audio: {
+            mandatory: {
+                chromeMediaSource: "desktop",
+                chromeMediaSourceId: id
+            }
+        },
+        video: {
+            mandatory: {
+                chromeMediaSource: "desktop",
+                chromeMediaSourceId: id,
+                maxFrameRate: 30
+//https://stackoverflow.com/questions/44353801/how-to-add-audio-in-desktop-capture-in-chrome-extension
+            }
+        }
+    }).then(startStream).catch(failedStream);
+}
+
+
+
+function startStream(stream) {
+    var video = document.getElementById('mainScreen');
+    //video.src = URL.createObjectURL(stream); //converts video binary code into URL
+    //^^^ above was deprecated (or soon will be)
+    // https://www.chromestatus.com/features/5618491470118912
+    try {
+        video.srcObject = stream;
+    } catch (error) {
+        video.src = URL.createObjectURL(stream);
+    }
+}
+
+function failedStream() {
+    console.log("Stream failure.");
+}
+
+// FIGURE OUT HOW TO CAPTURE THE MEDIA AND SAVE TO FILE
+// https://addpipe.com/blog/mediarecorder-api/
+// https://developer.mozilla.org/en-US/docs/Web/API/MediaStream_Recording_API
